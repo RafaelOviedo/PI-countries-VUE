4b86000b0fb6781f7255177acbe6d6b15b004312
Rafael Oviedo <oviedo.rp@gmail.com>
2021-07-27 23:24:41 -0300

    creating vuex store

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        allCountries: [],
+        countryId: {},
+    },
+
+    mutations: {},
+
+    actions: {},
+
+    getters: {},
+
+    modules: {},
+});
+
+export default store;
